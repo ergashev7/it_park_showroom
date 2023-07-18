c4fe306c74107385b55a6dee017f36f9537240b6
00012414 <wiut00012414@gmail.com>
2023-07-18 20:59:36 +0500

    new img file

--- src/main/login/login.jsx
+++ src/main/login/login.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import imgIT from "../../assets/img/it-park 1.png"
 import "./login.css"
 function Login() {
-  return (
-    <div className='container'>
-        <div className="bg-container">
-
+    return (
+        <div className='container'>
+            <div className="bg-container">
+                <div className="form">
+                    <div className="form-container">
+                        <div className="form-img">
+                            <img src={imgIT} width="300px" alt="" />
+                        </div>
+                        <div className="form-card">
+                            <p className='form-text'>Добро пожаловать <br /> в календарь</p>
+                            <div style={{width:"401px"}}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
