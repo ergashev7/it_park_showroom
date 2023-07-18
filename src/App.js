786f0c46b8d3add26e9ed0f891aa5ccd566abe8f
00012414 <wiut00012414@gmail.com>
2023-07-18 19:14:48 +0500

    new button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="App">
       salom
+      <Button title="salom" class="red lg"/>
     </div>
   );
 }
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+function Button(props) {
+  return (
+    <div>
+        <button className={props.class}>{props.title}</button>
+    </div>
+  )
+}
+
+export default Button
