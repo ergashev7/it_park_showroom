f71fb170f7fd1f11a6f0f74f352eb210c7889b47
ergashev7 <dior7354423@gmail.com>
2023-07-18 22:08:57 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import BasicDemo from "./components";
 import Button from "./components/Button";
 import Login from "./main/login/login";
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-import "primereact/resources/primereact.min.css";                                       
-        
+
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Login />
     </div>
   );
 }
