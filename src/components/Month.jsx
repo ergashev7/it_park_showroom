423edf018203afdd9787a85c7fe26ad37b9f25e5
00012414 <wiut00012414@gmail.com>
2023-07-20 00:56:04 +0500

    dahuya narsa

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -9,7 +9,7 @@ function Month({month}) {
                 <React.Fragment key={i}>
                     {
                         row.map((day,idx)=>(
-                            <Day day={day} key={idx}/>
+                            <Day day={day} key={idx} rowIdx={i}/>
                         ))
                     }
                 </React.Fragment>
