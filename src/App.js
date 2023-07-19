e5177fc0bf0dfa18a4368befdb21b211de5fe889
00012414 <wiut00012414@gmail.com>
2023-07-19 21:37:57 +0500

    add new admin page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Button from "./components/Button";
 import Login from "./main/login/login";
 import "primereact/resources/themes/lara-light-indigo/theme.css";     
 import "primereact/resources/primereact.min.css";                                       
+import Admin from "./main/adim";
         
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Admin/>
     </div>
   );
 }
