da67239267a0cc5e2b7ef95bed1cbf1455d1282a
00012414 <wiut00012414@gmail.com>
2023-07-20 22:12:14 +0500

    add new filter

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import CreateEventButton from './CreateEventButton'
-import SmallCalendar from './SmallCalendar'
-
-function Sidebar() {
+import React from "react";
+import CreateEventButton from "./CreateEventButton";
+import SmallCalendar from "./SmallCalendar";
+import Labels from "./Labels";
+export default function Sidebar() {
   return (
-   <aside className='border p-5 w-64'>
-      <CreateEventButton/>
-      <SmallCalendar/> 
-   </aside>
-  )
+    <aside className="border p-5 w-64">
+      <CreateEventButton />
+      <SmallCalendar />
+      <Labels />
+    </aside>
+  );
 }
-
-export default Sidebar
