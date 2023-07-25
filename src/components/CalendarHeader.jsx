0689f3c54d597f7bdb476c31e3886a477e51c44b
ergashev7 <dior7354423@gmail.com>
2023-07-25 23:14:36 +0500

    x ni mayaqqa otkzdm

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -18,36 +18,29 @@ function CalendarHeader() {
     );
   }
   return (
-    <header className="flex justify-between">
-      <div className=" flex items-center ">
-        <img src={logo} className="mr-2  h-12" alt="" />
+    <header className="px-4 py-2 flex items-center ">
+      <img src={logo} className="mr-2  h-12" alt="" />
 
-        <button
-          className="border border-gray-500 rounded py-1 text-center w-28 hover:bg-blue-500 hover:text-white "
-          onClick={handleReset}
-        >
-          {" "}
-          Сегодня{" "}
-        </button>
-        <button onClick={handlePrevMonth}>
-          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2 ">
-            chevron_left
-          </span>
-        </button>
-        <button onClick={handleNextMonth}>
-          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-            chevron_right
-          </span>
-        </button>
-        <h2 className="ml-4 text-xl text-gray-600 font-bold mx-2">
-          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
-        </h2>
-      </div>
-      <div className="px-1 py-2">
-        <button className="border border-gray-500 rounded py-1 text-center w-28 hover:bg-blue-500 hover:text-white ">
-          Новый
-        </button>
-      </div>
+      <button
+        className="border border-gray-500 rounded py-2 px-4 mr-5 "
+        onClick={handleReset}
+      >
+        {" "}
+        Today{" "}
+      </button>
+      <button onClick={handlePrevMonth}>
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_left
+        </span>
+      </button>
+      <button onClick={handleNextMonth}>
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_right
+        </span>
+      </button>
+      <h2 className="ml-4 text-xl text-gray-600 font-bold mx-2">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+      </h2>
     </header>
   );
 }
