a4fa0b9abc0200274a493bbc8715410445e25a2f
00012414 <wiut00012414@gmail.com>
2023-07-25 23:53:37 +0500

    dddd

--- src/main/admin.jsx
+++ src/main/admin.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import CalendarHeader from "../components/CalendarHeader"
+import CalendarHeader from "../components/CalendarHeader";
 import Month from "../components/Month";
 import Sidebar from "../components/Sidebar";
 import { getMonth } from "../util";
@@ -8,26 +8,28 @@ import EventModal from "../components/EventModal";
 function Admin() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
-  const [hidden , setHidden] =useState()
+  const [hidden, setHidden] = useState(true);
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
   }, [monthIndex]);
   function onHidden() {
-    setHidden(!hidden)
+    setHidden(!hidden);
   }
   return (
     <React.Fragment>
-        {showEventModal && <EventModal />}
-        <div className="h-screen flex flex-col">
-          <div className="flex justify-between">
+      {showEventModal && <EventModal />}
+      <div className="h-screen flex flex-col">
+        <div className="flex justify-between">
           <CalendarHeader />
-          <button onClick={onHidden} >saa</button>
-          </div>
-          <div className="flex flex-1">
-            <Month month={currentMonth} />
-            <Sidebar showHidden={hidden} />
-          </div>
+          <button onClick={onHidden}>
+            <span className="material-icons-outlined text-black text-3xl px-6">menu</span>
+          </button>
         </div>
+        <div className="flex flex-1 ">
+          <Month month={currentMonth} />
+          <Sidebar showHidden={hidden} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
